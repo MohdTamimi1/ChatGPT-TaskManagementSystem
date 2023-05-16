32377bcb32fbf3b3cf79fb2069aeaf5f1b5a67cc
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-05-16 05:06:29 -0400

    Serve the plugin manifest

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@ const PORT = 3000;
 
 // Enable CORS for all routes
 app.use(cors());
+
+// Serve the plugin manifest
+app.get("/.well-known/ai-plugin.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", ".well-known", "ai-plugin.json"));
+});
