00ccc9242da2c1a3d8b4e4fef4c24508472d269a
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-05-25 10:15:59 -0400

    edit task added

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { addTask, getTasks } = require("./controllers");
+const { addTask, getTasks, editTask } = require("./controllers");
 
 // Add a new task
 router.post("/tasks", addTask);
