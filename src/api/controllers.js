bb48b04e33048aaf08046cf67c347b20034570d2
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-05-26 10:25:43 -0400

    Delete task function

--- src/api/controllers.js
+++ src/api/controllers.js
@@ -12,5 +12,17 @@ const getTasks = (req, res) => {
   res.status(200).json({ tasks });
 };
 
+// Delete task
+const deleteTask = (req, res) => {
+  const taskIndex = req.params.taskIndex;
+
+  if (taskIndex >= 0 && taskIndex < tasks.length) {
+    tasks.splice(taskIndex, 1);
+    res.status(200).json({ message: "Task deleted successfully." });
+  } else {
+    res.status(400).json({ message: "Invalid task index." });
+  }
+};
+
 // Export functions
 module.exports = { addTask, getTasks };
