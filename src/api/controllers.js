a1b313904e9ac69b0fcbd15ca406b088afb233fc
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-05-26 10:14:01 -0400

    removed edit tasks

--- src/api/controllers.js
+++ src/api/controllers.js
@@ -12,18 +12,5 @@ const getTasks = (req, res) => {
   res.status(200).json({ tasks });
 };
 
-// Edit task
-const editTask = (req, res) => {
-  const taskIndex = req.params.taskIndex;
-  const newTask = req.body.newTask;
-
-  if (taskIndex >= 0 && taskIndex < tasks.length) {
-    tasks[taskIndex] = newTask;
-    res.status(200).json({ message: "Task edited successfully." });
-  } else {
-    res.status(400).json({ message: "Invalid task index." });
-  }
-};
-
 // Export functions
 module.exports = { addTask, getTasks, editTask };
