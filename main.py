81a523644c0f1fab71bc9ff92ce1b57e817d0d73
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-06-01 10:45:32 -0400

    Imports

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import quart
 import quart_cors
 from quart import request
 
-app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+app = quart_cors.cors(quart.Quart(__name__),
+                      allow_origin="https://chat.openai.com")
