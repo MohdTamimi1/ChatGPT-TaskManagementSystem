8ddb877fc63e2e565d9a126ed14f8777a26c2a9f
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-06-02 09:51:13 -0400

    get logo, ai-plugin, openapi.yaml

--- main.py
+++ main.py
@@ -32,3 +32,33 @@ async def delete_todo(username):
     if 0 <= todo_idx < len(_TODOS[username]):
         _TODOS[username].pop(todo_idx)
     return quart.Response(response='OK', status=200)
+
+
+@app.get("/logo.png")
+async def plugin_logo():
+    filename = 'logo.png'
+    return await quart.send_file(filename, mimetype='image/png')
+
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    host = request.headers['Host']
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/json")
+
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    host = request.headers['Host']
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/yaml")
+
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5003)
+
+
+if __name__ == "__main__":
+    main()
